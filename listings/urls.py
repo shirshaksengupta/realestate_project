c277865d3c5a4daa130cfae516a72510a725ced0
Shirshak Sengupta <shirshaksengupta@Shirshaks-MacBook-Air.local>
2021-12-24 17:06:55 +0530

    Front end views, search, pagination, etc all done.

--- listings/urls.py
+++ listings/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='listings'),
-    path('<int:listing_id>', views.listing, name='listing'),
+    path('<int:listing_id>', views.listing, name='listing'), # Passing listing_id to listing.html
     path('search', views.search, name='search'),
 ]
