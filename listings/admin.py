7b66bac86bdcb196c3ed6bcb302f369cccde14ac
Shirshak Sengupta <shirshaksengupta@Shirshaks-MacBook-Air.local>
2021-12-24 13:11:18 +0530

    Created a superuser to access the admin site and added a few listings and realtors to the DB using the site. Customized the admin site using CSS.

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Listing
+
+# Creating a class to change the display properties of Listing in admin site
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title') # Can go to the listing properties using either id or title
+    list_filter = ('realtor',) # Filter by realtor
+    list_editable = ('is_published',) # Could be edited via the site
+    search_fields = ('title', 'description', 'adress', 'city', 'state', 'zipcode', 'price') # Get a search field
+    list_per_page = 25 # Pagination
+ 
+admin.site.register(Listing, ListingAdmin)
--- realtors/admin.py
+++ realtors/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Realtor
+
+# Creating a class that will change display properties of Realtor in admin site
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 25
+
+# This will register the Realtor model in the Django admin site
+admin.site.register(Realtor, RealtorAdmin)
