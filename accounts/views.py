faaed68406d4901ad529041770c59650e9b9f9c9
Shirshak Sengupta <shirshaksengupta@Shirshaks-MacBook-Air.local>
2021-12-27 12:47:59 +0530

    Added user registration/login skeletal, and bootstrap error messaging to clients

---
+++ accounts/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+def register(request):
+    # Getting this post from register.html line 15
+    if request.method == 'POST':
+        messages.error(request, 'Testing error message')
+        return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
+
+def login(request):
+    if request.method == 'POST':
+        # Login User
+        return
+    else:
+        return render(request, 'accounts/login.html')
+
+def logout(request):
+    return redirect('index')
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
